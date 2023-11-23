6c977dbff96df5a29dccd91874f997ab2512ef37
Mahabubul Hasan <shourav6688@gmail.com>
2023-11-24 00:23:21 +0600

    chore: added url to travel decision

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from api.views import GetCoolestDistrictsAPIView
+from api.views import GetCoolestDistrictsAPIView, TravelDeciderAPIView
 
 urlpatterns = [
 
     path("v1/get-coolest-districts/", GetCoolestDistrictsAPIView.as_view(), name="coolest-districts"),
+    path("v1/travel-decider/", TravelDeciderAPIView.as_view(), name="travel-decider"),
 ]
 
 
