e753e73c8856f70a3518ca24736d94c8cdd05f9a
Mahabubul Hasan <shourav6688@gmail.com>
2023-11-23 23:21:37 +0600

    feat: updated data parser for to optimize response time

--- utility/data_parser.py
+++ utility/data_parser.py
@@ -1,20 +1,5 @@
 def parse_forecasted_data(forecast_data):
-    index_of_2pm_temp = {}
-
-    for i in forecast_data:
-        index_arr = []
-        for j in range(len(forecast_data[i]["date"])):
-            if forecast_data[i]["date"].time[j].hour == 14:
-                index_arr.append(j)
-        index_of_2pm_temp[i] = index_arr
-
-    average_temp_dict_at_2pm = {}
-    for i in index_of_2pm_temp:
-        sum_of_temperature = 0
-        current_index = index_of_2pm_temp[i]
-        for j in current_index:
-            sum_of_temperature += forecast_data[i]["temperature_2m"][j]
-        average_temp_dict_at_2pm[i] = sum_of_temperature / 7
-
+    index_of_2pm_temp = {key: [j for j, date in enumerate(value["date"]) if date.time().hour == 14] for key, value in forecast_data.items()}
+    average_temp_dict_at_2pm = {key: sum(forecast_data[key]["temperature_2m"][j] for j in index_of_2pm_temp[key]) / len(index_of_2pm_temp[key]) for key in index_of_2pm_temp}
     coolest_districts = dict(sorted(average_temp_dict_at_2pm.items(), key=lambda item: item[1]))
     return dict(list(coolest_districts.items())[:10])
